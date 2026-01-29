8fd551b7c772e52a59dc437ef6f2ac6d32b0d095
Alexandre CG Jr <alexandrecgjunior@gmail.com>
2026-01-29 15:03:56 -0300

    feat: gerando reports

--- TesteSeleniumBasico/src/test/java/runner/RunCucumberTest.java
+++ TesteSeleniumBasico/src/test/java/runner/RunCucumberTest.java
@@ -8,29 +8,27 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.swing.plaf.synth.SynthTextAreaUI;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {},
+        plugin = {
+                "pretty",
+                "json:target/reports/cucumber.json",
+                "html:target/reports/cucumber.html"
+        },
         features = "src/test/resources/features",
         tags = {"~@Ignore"},
         glue = {"steps"}
-
 )
 public class RunCucumberTest {
-
     public static WebDriver driver;
 
     @BeforeClass
     public static void start() {
         driver = new ChromeDriver();
-
     }
 
     @AfterClass
     public static void stop() {
         driver.quit();
-
     }
 }
