2bb29908dad49321adfdf1187ee086a670864189
Alexandre CG Jr <alexandrecgjunior@gmail.com>
2026-01-29 10:16:44 -0300

    feat: adicionando page objects

---
+++ TesteSeleniumBasico/src/test/java/pages/HomePage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void acessarSite() {
+        driver.get("https://qazando.com.br/curso.html");
+        Assert.assertEquals("Não acessou a aplicação", true, driver.findElement(By.id("btn-ver-cursos")).isDisplayed());
+    }
+
+    public void scrollDown() throws InterruptedException {
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,10000)", "");
+        Thread.sleep(2000);
+    }
+
+    public void PreencherEmail() {
+        driver.findElement(By.id("email")).sendKeys("alexandre@teste.com");
+    }
+
+    public void GanharCupom() {
+        driver.findElement(By.id("button")).click();
+    }
+
+    public void verificarCupom() {
+        String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+        Assert.assertEquals("O cupom está errado!", "QAZANDO15OFF", texto_cupom);
+    }
+}
--- TesteSeleniumBasico/src/test/java/steps/DescontosStep.java
+++ TesteSeleniumBasico/src/test/java/steps/DescontosStep.java
@@ -3,46 +3,33 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import runner.RunCucumberTest;
 
 
 public class DescontosStep extends RunCucumberTest {
 
-
+    HomePage homePage = new HomePage(driver);
 
     @Dado("^que estou no site da qazando")
     public void acessar_site_qazando() {
-        driver.get("https://qazando.com.br/curso.html");
-        Assert.assertEquals("Não acessou a aplicação", true, driver.findElement(By.id("btn-ver-cursos")).isDisplayed());
+        homePage.acessarSite();
     }
 
     @Quando("^preencho meu email$")
     public void preencho_meu_email() throws InterruptedException {
-
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        jse.executeScript("window.scrollBy(0,10000)", "");
-        Thread.sleep(2000);
-
-        driver.findElement(By.id("email")).sendKeys("alexandre@teste.com");
+        homePage.scrollDown();
+        homePage.PreencherEmail();
     }
 
     @Quando("^clico em ganhar cupom$")
     public void clico_em_ganhar_cupom() {
-        driver.findElement(By.id("button")).click();
+        homePage.GanharCupom();
     }
 
     @Então("^eu vejo o código de desconto$")
     public void eu_vejo_o_código_de_desconto() {
-        String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
-        System.out.println(texto_cupom);
-
-        Assert.assertEquals("O cupom está errado!", "QAZANDO15OFF", texto_cupom);
-
+        homePage.verificarCupom();
     }
 
 }
